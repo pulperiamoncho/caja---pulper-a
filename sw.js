c888a844dbeda2770306ea6127ba94153f4fa473
pulperiamoncho <cherlin.corella@gmail.com>
2026-08-29 14:41:48 -0600

    Add service worker for caching and fetch handling

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE = "caja-pulperia-v8";
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache =>
+      cache.addAll(["./", "./index.html", "./manifest.json"])
+    )
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response =>
+      response || fetch(event.request)
+    )
+  );
+});
